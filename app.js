3e4a1c984882266623e5310333e7fd83314d8607
João Guedes <joaoguedes1404@gmail.com>
2024-06-24 12:53:48 -0300

    Adicionando nodemon e iniciando servidor

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const express = require("express");
 
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("<h1>Minha lista de tarefas</h1>");
+});
+
+app.listen(8081, () => {
+    console.log("O servidor foi iniciado!");
+});
