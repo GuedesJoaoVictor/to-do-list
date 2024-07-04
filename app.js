4d84810f07d4372fa8749e9de6fdf5f6b80f0e53
João Guedes <joaoguedes1404@gmail.com>
2024-07-04 11:21:58 -0300

    Adicionando mongoose ao projeto.

--- app.js
+++ app.js
@@ -1,24 +1,12 @@
 const express = require("express");
+const checklistRouter = require("./src/routes/checklist");
+require("./config/database");
 
 const app = express();
 
 app.use(express.json());
 
-const log = (req, res, next) => {
-    console.log(req.body);
-    console.log(`Data de agora: ${Date.now()}`);
-    next();
-}
-
-app.use(log); 
-
-app.get("/", (req, res) => {
-    res.send("<h1>Minha lista de tarefas :)</h1>");
-});
-
-app.get("/json", (req, res) => {
-    res.json({title: "Tarefa X", done: true});
-});
+app.use("/checklists", checklistRouter);
 
 app.listen(8081, () => {
     console.log("O servidor foi iniciado!");
